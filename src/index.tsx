d154ab5f2c9a367b6fd9124f975f0a6fe900221c
Ilnura <t.ilnura@gmail.com>
2023-01-20 17:42:05 -0700

    Add project to GitHub

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const client = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+        <App />
+    </QueryClientProvider>
+  
   </React.StrictMode>
 );
 
