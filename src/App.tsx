15b79a2779734055a140c6b314a61c9c07d70e5c
Ilnura <t.ilnura@gmail.com>
2023-01-25 14:33:20 -0700

    cart and add, remove func-s

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,35 @@ const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProdu
 const getTotalItems = (items: CartItemType[]) => 
  items.reduce((ack: number, item ) => ack + item.amount, 0);
 
-const handleAddToCart = (clickedItem: CartItemType) => null;
+const handleAddToCart = (clickedItem: CartItemType) => {
+  setCartItems(prev => {
+    // 1. Is the item already in the cart?
+    const isItemInCart = prev.find(item => item.id === clickedItem.id)
 
-const  handleRemoveFromCart = () => null;
+    if (isItemInCart) {
+      return prev.map(item => 
+        item.id === clickedItem.id 
+        ? { ...item, amount: item.amount + 1}
+        : item
+      );
+    }
+    //First Time the item added
+    return [...prev, { ...clickedItem, amount: 1 }]
+  })
+};
+
+const  handleRemoveFromCart = (id: number) => {
+setCartItems(prev => (
+  prev.reduce((ack, item) => {
+    if(item.id === id) {
+      if(item.amount === 1) return ack;
+      return [...ack, { ...item, amount: item.amount - 1}];
+    } else {
+      return [...ack, item]
+    }
+  }, [] as CartItemType[]) 
+))
+};
 
 if (isLoading) return <LinearProgress />
 if (error) return <div>Something went wrong ...</div>
